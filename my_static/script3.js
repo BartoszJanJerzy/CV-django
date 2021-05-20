3187304466bfc88f8e775aea94f30e09ef537706
TytanowyBartosz <bartoszjanjerzy@gmail.com>
2021-05-20 19:32:22 +0200

    20-05

---
+++ my_static/script3.js
@@ -0,0 +1,20 @@
+$('#boredom-script').hover(function(){
+    $('#boredom-body').show(500);
+})
+$('#boredom-script').on('mouseleave', function(){
+    $('#boredom-body').hide(500);
+})
+
+$('#ocr-app').hover(function(){
+    $('#ocr-app-body').show(500);
+})
+$('#ocr-app').on('mouseleave', function(){
+    $('#ocr-app-body').hide(500);
+})
+
+$('#ih-grant').hover(function(){
+    $('#ih-grant-body').show(500);
+})
+$('#ih-grant').on('mouseleave', function(){
+    $('#ih-grant-body').hide(500);
+})
