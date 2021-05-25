097017cb18432bbcf3c7eb13590d27fa0b05f8c3
TytanowyBartosz <bartoszjanjerzy@gmail.com>
2021-05-25 17:53:36 +0200

    25.05

--- my_static/script3.js
+++ my_static/script3.js
@@ -12,6 +12,27 @@ $('#ocr-app').on('mouseleave', function(){
     $('#ocr-app-body').hide(500);
 })
 
+$('#regression-app').hover(function(){
+    $('#regression-app-body').show(500);
+})
+$('#regression-app').on('mouseleave', function(){
+    $('#regression-app-body').hide(500);
+})
+
+$('#xml-data').hover(function(){
+    $('#xml-data-body').show(500);
+})
+$('#xml-data').on('mouseleave', function(){
+    $('#xml-data-body').hide(500);
+})
+
+$('#lime-tuning').hover(function(){
+    $('#lime-tuning-body').show(500);
+})
+$('#lime-tuning').on('mouseleave', function(){
+    $('#lime-tuning-body').hide(500);
+})
+
 $('#ih-grant').hover(function(){
     $('#ih-grant-body').show(500);
 })
